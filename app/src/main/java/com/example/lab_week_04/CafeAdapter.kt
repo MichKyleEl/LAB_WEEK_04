bf36c6f34766a9c2da8936d1acfb4fd4c53be352
MichKyleEl <164639246+MichKyleEl@users.noreply.github.com>
2025-09-16 12:21:49 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,8 +1,6 @@
 package com.example.lab_week_04
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 val TABS_FIXED = listOf(
@@ -11,13 +9,20 @@ val TABS_FIXED = listOf(
     R.string.kopikenangan_title,
 )
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+// Ganti konstruktor agar menerima Fragment sebagai parameter
+class CafeAdapter(private val fragment: Fragment) : FragmentStateAdapter(fragment) {
+
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+
+    override fun createFragment(position: Int): Fragment {
+        val content = when (position) {
+            0 -> fragment.resources.getString(R.string.starbucks_desc)
+            1 -> fragment.resources.getString(R.string.janjijiwa_desc)
+            2 -> fragment.resources.getString(R.string.kopikenangan_desc)
+            else -> ""
+        }
+        return CafeDetailFragment.newInstance(content)
     }
 }
